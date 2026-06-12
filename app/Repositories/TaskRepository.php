000743901ce2d3b558fb520ba8437c0519775e98
Adiksuu <codeadiksuu@gmail.com>
2026-06-12 20:57:06 +0200

    TaskRepository z funkcjami operującymi na Task

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Collection;
+
+class TaskRepository
+{
+    public function getAll(): Collection {
+        return Task::latest()->get();
+    }
+    public function findById(int $id): ?Task {
+        return Task::find($id);
+    }
+    public function create(array $data): Task {
+        return Task::create($data);
+    }
+    public function update(Task $task, array $data): Task {
+        return $task->update($data);
+    }
+    public function delete(Task $task): bool {
+        return $task->delete();
+    }
+}
